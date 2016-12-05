f8f39cd85ea4ccfd56aad9f36d6206bf50fbade5
johansteiner <johan.steiner@he-arc.ch>
2016-12-05 14:23:29 +0100

    tests

--- ODIepicearc/src/test/java/ch/hearc/ig/indu/epicearc/business/TestOrder.java
+++ ODIepicearc/src/test/java/ch/hearc/ig/indu/epicearc/business/TestOrder.java
@@ -5,10 +5,38 @@
  */
 package ch.hearc.ig.indu.epicearc.business;
 
+import ch.hearc.ig.odi.epicearc.business.ConiferSize;
+import ch.hearc.ig.odi.epicearc.business.ConiferType;
+import ch.hearc.ig.odi.epicearc.business.Customer;
+import ch.hearc.ig.odi.epicearc.business.DeliveryDate;
+import ch.hearc.ig.odi.epicearc.business.Order;
+import ch.hearc.ig.odi.epicearc.business.PickupDate;
+import ch.hearc.ig.odi.epicearc.business.Product;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 /**
  *
  * @author johan.steiner
  */
 public class TestOrder {
+        
+    @Test
+    public void TestOrderNotNull() {
+        Customer customer = new Customer(Long.valueOf(1), "Johan", "Steiner", "Clos Beausan 1", "2736", "Sorvilier", "0324921211", "johan.steiner@he-arc.ch");
+        Product product = new Product(Long.valueOf(1), Float.valueOf(100), ConiferType.SPRUCE, ConiferSize.MINI);
+        DeliveryDate dateDeliv = new DeliveryDate(Long.valueOf(1), "15.05.13", ConiferType.SPRUCE);
+        PickupDate datePickup = new PickupDate(Long.valueOf(1), "18.05.15", ConiferType.SPRUCE);
+        
+        Order order = new Order(Long.valueOf(1), "13.05.13", Float.valueOf(100), dateDeliv, datePickup, product, customer);
+    }
+    
+    @Test
+    public void TestNewOrderGetterSetter() {
+        
+        Order order = new Order();
+        
+        assertFalse(order==null);
+    }
     
 }
--- ODIepicearc/src/test/java/ch/hearc/ig/indu/epicearc/business/TestProduct.java
+++ ODIepicearc/src/test/java/ch/hearc/ig/indu/epicearc/business/TestProduct.java
@@ -5,6 +5,8 @@
  */
 package ch.hearc.ig.indu.epicearc.business;
 
+import ch.hearc.ig.odi.epicearc.business.ConiferSize;
+import ch.hearc.ig.odi.epicearc.business.ConiferType;
 import ch.hearc.ig.odi.epicearc.business.Product;
 import static org.junit.Assert.*;
 import org.junit.Test;
@@ -15,6 +17,25 @@ import org.junit.Test;
  */
 public class TestProduct {
     
-    
+    @Test
+    public void TestNewProductGetterSetter() {
+
+        ConiferSize size = ConiferSize.BIG;
+        ConiferType type = ConiferType.NORDMANN;
+        Product prod = new Product();
+        
+        assertFalse(prod == null);
+        
+        prod.setId(Long.valueOf(1));
+        prod.setPrice(Float.valueOf(100));
+        prod.setConiferSize(size);
+        prod.setConiferType(type);
+        
+        assertEquals(prod.getId(), new Long(1));
+        assertEquals(prod.getPrice(), new Float(100));
+        assertEquals(prod.getConiferSize().getCode().toUpperCase(), size.getCode().toUpperCase());
+        assertEquals(prod.getConiferType().getCode().toUpperCase(), type.getCode().toUpperCase());
+        
+    }
     
 }
