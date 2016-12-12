fa9c1805ef9cec35355ed9a61e2fd23c071f54ee
quiquerezsebastien <sebastien.quiquerez@he-arc.ch>
2016-12-12 14:37:40 +0100

    add class suite test

---
+++ ODIepicearc/src/test/java/ch/hearc/ig/indu/epicearc/business/FeatureTestSuite.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.indu.epicearc.business;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author sebastie.quiquere
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  TestConiferSize.class,
+  TestConiferType.class,
+  TestCustomer.class,
+  TestDate.class,
+  TestOrder.class,
+  TestProduct.class
+})
+
+public class FeatureTestSuite {
+    
+}
